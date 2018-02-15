a0b9366c6706f25c7f0b21bf0c75fd556b602175
AnnaLovisa <gribbe.anna@gmail.com>
2018-02-15 13:56:04 +0100

    add Skeleton and create functional components

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () =>  {
+    return (
+      <div className="NavBar">
+        <div className="row">
+          <div className="">test</div>
+          <div className="">test</div>
+          <div className="">test</div>
+        </div>
+      </div>
+    );
+}
+
+export default NavBar;
