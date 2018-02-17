68c9c9d199886aa03bb6cf1e89c623cb2f9e0891
AnnaLovisa <gribbe.anna@gmail.com>
2018-02-17 15:03:52 +0100

    add content in components and delete skeleton and change name of components

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Header = () => {
     return (
-      <div className="container">
         <div className="row">
+          <header className="col-xs-6">
+            <h1>FBA Research Working Groups</h1>
+            <p>A database of members of FBA's research working groups</p>
+          </header>
         </div>
-      </div>
     );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const NavBar = () =>  {
     return (
-      <div className="NavBar">
-        <strong>Display</strong>
-        <div className="btn-group">
-            <a href="" id="list" className="btn btn-default btn-sm"><i className="fa fa-th-list"></i>
-                List</a> <a href="" id="grid" class="btn btn-default btn-sm">
+      <div className="row">
+        {/*This should only appear for admins*/}
+        <div className="col-xs-2 pull-left">
+            <a href="" id="addlink" className="btn">
+                Add new researcher</a>
+        </div>
+        <div className="col-xs-2 pull-right"><strong>Display</strong>
+            <a href="" id="list" className="btn"><i className="fa fa-th-list"></i>
+                List</a> <a href="" id="grid" className="btn">
                 <i className="fa fa-th"></i>Grid</a>
         </div>
       </div>
