36a43c6ff61da43061e160a44ba3be413e876d06
AnnaLovisa <gribbe.anna@gmail.com>
2018-02-19 13:04:54 +0100

    init redux and add redux in package.json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Footer = () =>  {
+
     return (
       <div className="row">
-        <div className="col-sm-2 offset-sm-10 pr-4">
-            <img src={`../img/fba_logo_pos_rgb.png`} className="img-fluid" alt={`FBA logotype`} />
+        <div className="col-sm-2 offset-sm-10 pr-4 pull-right">
+            <img src={require(`../img/fba_logo_pos_rgb.png`)} className="img-fluid" alt="FBA logotype" height={"100px"} width={"auto"} />
         </div>
         <div className="col-sm-2">
             <p>Anna Gribbe - February 2018</p>
