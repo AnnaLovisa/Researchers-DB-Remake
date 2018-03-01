ce57da0cb7e1c2088b640cee89ecfd0bd86b9094
AnnaLovisa <gribbe.anna@gmail.com>
2018-03-01 22:06:41 +0100

    add more redux form-fields

--- src/components/add_form.js
+++ src/components/add_form.js
@@ -3,21 +3,82 @@ import { Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import Button from '../components/button';
 import TextField from '../components/text_field';
+import SelectMenu from '../components/select_menu';
+import RadioButton from '../components/radio_button';
 
 
 export const AddFormComponent = ({ handleSubmit, onSubmit }) => {
   return (
-    <div>
-      <h1>My Very own Form</h1>
+    <div className="d-flex flex-column justify-content-center items-center">
+      <h4>Add new researcher</h4>
       <form onSubmit={handleSubmit(onSubmit)}>
+        {/* <button type="submit">Submit</button> */}
+        <Button type="submit" buttonLabels="Submit" />
+        <Link to="/">Cancel</Link>
         <Field
           name="name"
           label="Name"
           component={TextField}
         />
-        {/* <button type="submit">Submit</button> */}
-        <Button type="submit" buttonLabels="Submit" />
-        <Link to="/">Cancel</Link>
+        <Field
+          name="institution"
+          label="Institution"
+          component={TextField}
+        />
+        <Field
+          name="gender"
+          label="Gender"
+          component={RadioButton}
+          options={{
+            male: 'Male',
+            female: 'Female'
+          }}
+        />
+        <Field
+          name="year"
+          label="Year of PhD"
+          component={SelectMenu}
+          options={{
+            1325: '1325',
+            pko: 'PKO',
+            prevention: 'Prevention'
+          }}
+        />
+        <Field
+          name="website"
+          label="Add website address: "
+          component={TextField}
+        />
+        <Field
+          name="workingGroup"
+          label="Working Group"
+          component={SelectMenu}
+          options={{
+            1325: '1325',
+            pko: 'PKO',
+            prevention: 'Prevention'
+          }}
+        />
+        <Field
+          name="regionOfExpertise"
+          label="Region of Expertise"
+          component={SelectMenu}
+          options={{
+            afghanistan: 'Afghanistan',
+            africa: 'Africa',
+            asiaPacificRegion: 'Asia-Pacific Region'
+          }}
+        />
+        <Field
+          name="fieldOfResearch"
+          label="Field of Research"
+          component={SelectMenu}
+          options={{
+            conflicts: 'Conflicts',
+            ddr: 'DDR',
+            gender: 'Gender'
+          }}
+        />
       </form>
     </div>
   );
---
+++ src/components/select_menu.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const SelectMenu = props => {
+
+  const renderSelectOptions = (key, index) => {
+    return (
+      <option
+        key={`${index}-${key}`}
+        value={key}
+      >
+        {props.options[key]}
+      </option>
+    );
+  }
+
+  if (props && props.options) {
+    return (
+      <div>
+        <div>{props.label}</div>
+        <select {...props.input}>
+          <option value="">Select</option>
+          {Object.keys(props.options).map(renderSelectOptions)}
+        </select>
+      </div>
+    )
+  }
+  return <div></div>
+}
+
+export default SelectMenu;
